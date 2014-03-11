760866cd2eb5f2a4d80ef74762476fcb8a02ea32
Astr0surf3r <astr0surf3r@gmail.com>
2014-03-11 23:11:49 +0100

    cree la relacion intra entre Imagen y Articulo 1:1

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,3 +1,6 @@
 class Articulo < ActiveRecord::Base
   attr_accessible :cuerpo, :titulo
+
+  has_one :imagen
+  
 end
--- app/models/imagen.rb
+++ app/models/imagen.rb
@@ -1,3 +1,6 @@
 class Imagen < ActiveRecord::Base
   attr_accessible :articulo_id, :nombre, :ruta
+  
+  belongs_to :articulo
+
 end
